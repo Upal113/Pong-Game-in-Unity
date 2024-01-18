b7838bce57a8d6c110f3958a17e9cdb8f6c07e0f
Upal <71349329+Upal113@users.noreply.github.com>
2024-01-17 18:44:32 -0800

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -8,13 +7,15 @@ public class Ball : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        Launch();
+        // Use StartCoroutine to start the coroutine
+        StartCoroutine(DeleteCanvasAfterDelay());
     }
 
-    //Reset each time player scores
+    // Reset each time player scores
     public void Reset()
     {
         rb.velocity = Vector2.zero;
@@ -25,8 +26,31 @@ public class Ball : MonoBehaviour
     // Initially Launch the Ball
     private void Launch()
     {
-        float x = Random.Range(0,2) == 0 ? -1 :1;
-        float y = Random.Range(0,2) == 0 ? -1 :1;
-        rb.velocity = new Vector2(speed* x, speed * y);
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+
+    IEnumerator DeleteCanvasAfterDelay()
+    {
+        Debug.Log("Coroutine started");
+        // Wait for 10 seconds
+        yield return new WaitForSeconds(10f);
+
+        // Find the Canvas with the specified tag
+        GameObject canvas = GameObject.FindGameObjectWithTag("Help Screen");
+
+        // Check if the Canvas is found
+        if (canvas != null)
+        {
+            // Destroy the Canvas GameObject
+            Destroy(canvas);
+        }
+        else
+        {
+            Debug.LogError("Canvas with tag '" + "Help Screen" + "' not found!");
+        }
+        // Call Launch after starting the coroutine
+        Launch();
     }
 }
